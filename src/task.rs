933ad5b7eb1dc171a0ae59f949cf742b7ae1fb21
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-30 12:18:58 +0000

    create a new task to get the assigned id

---
+++ src/task.rs
@@ -0,0 +1,41 @@
+//! The fundamental `Task` building block and related functions.
+
+use std::borrow::Cow;
+
+use anyhow::{Ok, Result};
+
+/// A Task
+#[allow(dead_code)]
+struct Task {
+    name: Cow<'static, str>,
+    id: Option<u32>,
+    description: Option<Cow<'static, str>>,
+}
+
+impl Task {
+    /// Create a new task in the selected storage backend.
+    /// Returned Result should include the task ID.
+    #[allow(dead_code)]
+    fn create(&mut self) -> Result<&mut Self> {
+        self.id = Some(1);
+        Ok(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_task() {
+        let mut new_task: Task = Task {
+            name: "Test Task 1".into(),
+            id: None,
+            description: None,
+        };
+        let _ = new_task.create();
+        assert_eq!(new_task.name, "Test Task 1");
+        assert_eq!(new_task.description, None);
+        assert_eq!(new_task.id, Some(1));
+    }
+}
