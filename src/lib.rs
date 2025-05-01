7184de9dcb646a492f996495d1b7cea04cdd194b
Mike Foster <i.am.mike.foster@gmail.com>
2025-05-01 09:30:41 +0000

    restructure into bin & lib

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! The actual HelixFlow implementation. This provides all the building blocks and functionality
+//! needed for the app.
+
+mod backends {
+    mod surreal;
+}
+mod task;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod surreal;
-mod task;
+//! This is where the executable runtime invokation lives. It will make a lot of use of the
+//! HelixFlow library ;-)
 
 fn main() {
     println!("Hello, world!");
