922f5aa705d366540a9a434fc9f382ea79066241
Mike Foster <i.am.mike.foster@gmail.com>
2025-05-01 16:33:09 +0000

    fmt

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() {
     // Set SLINT_EMIT_DEBUG_INFO=1 for tests, rust-analyzer, etc.
-    if std::env::var("DEBUG") == Ok("true".to_string())
-    {
+    if std::env::var("DEBUG") == Ok("true".to_string()) {
         println!("cargo:rustc-env=SLINT_EMIT_DEBUG_INFO=1");
     }
 }
