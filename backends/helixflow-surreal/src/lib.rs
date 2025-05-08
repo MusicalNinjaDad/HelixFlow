3953e36437b22799c70391d6e7c6b2fd6eb8a547
Mike Foster <i.am.mike.foster@gmail.com>
2025-05-08 17:51:37 +0000

    surreal::blocking task creation works (ugly!)

--- backends/helixflow-surreal/src/lib.rs
+++ backends/helixflow-surreal/src/lib.rs
@@ -11,14 +11,19 @@ use surrealdb::{
         remote::ws::{Client, Ws},
     },
     opt::auth::Root,
-    sql::Thing,
+    sql::{Thing, Uuid, Id},
 };
+
+use serde::{Deserialize, Serialize};
+
 
 use helixflow_core::task::Task;
 
 pub mod blocking {
+    use std::borrow::Cow;
+
     use super::*;
-    use helixflow_core::task::blocking::StorageBackend;
+    use helixflow_core::task::{TaskResult, blocking::StorageBackend};
     /// An instance of a SurrealDb ready to use as a `StorageBackend`
     ///
     /// This requires some form of instantiation function, the exact specification of which will depend
@@ -36,33 +41,49 @@ pub mod blocking {
         rt: Rc<tokio::runtime::Runtime>,
     }
 
+    #[derive(Debug, Serialize, Deserialize)]
+    struct TaskNoId {
+        name: Cow<'static, str>,
+        id: Thing,
+        description: Option<Cow<'static, str>>,
+    }
+
     /// SurrealDb returns a `Thing` as `id`.
     ///
     /// A `Thing` is a wierd SurrealDb Struct with a `tb` (= "table") and `id` field,
     /// both as owned `String`s :-x (!!)
-    impl<C: Connection> StorageBackend<Thing> for SurrealDb<C> {
-        fn create(&self, task: &mut Task<Thing>) -> Result<()> {
-            let dbtask: Task<Thing> = self
+    impl<C: Connection> StorageBackend for SurrealDb<C> {
+        fn create(&self, task: &Task) -> anyhow::Result<Task> {
+            dbg!(task);
+            let taskuuid: Uuid = task.id.into();
+            let taskid: Id = taskuuid.into();
+            let taskthing: Thing = Thing::from(("Tasks", taskid));
+            let t = TaskNoId {name: task.name.clone(), id: taskthing, description: task.description.clone()};
+            let dbtask: TaskNoId = self
                 .rt
-                .block_on(self.db.create("Tasks").content(task.clone()).into_future())
-                .unwrap()
+                .block_on(self.db.create("Tasks").content(t).into_future())?
                 .with_context(|| format!("Creating new record for {:#?} in SurrealDb", task))?;
             // TODO: Wrangle id (into a couple of `Cow`s?) so it can be passed directly to
             // `.select<O>(&self, resource: impl surrealdb::opt::IntoResource<O>)` without
             // ownership and conversion concerns. (Or only partially, to avoid taking time now, and take
             // the time to clone/convert only when needed, e.g. on the first attempt to select?)
-            task.id = dbtask.id;
-            Ok(())
+            let checktask = Task {
+                name: dbtask.name,
+                id: if let Id::Uuid(dbuuid) = dbtask.id.id {dbuuid.into()} else {Uuid::new_v7().into()},
+                description: dbtask.description
+            };
+            dbg!(&checktask);
+            Ok(checktask)
         }
-        fn get(&self, id: Thing) -> Result<Task<Thing>> {
-            self.rt
-                .block_on(
-                    self.db
-                        .select((id.tb.clone(), id.id.to_raw()))
-                        .into_future(),
-                )?
-                .ok_or_else(|| anyhow!("Invalid task ID: {}", id))
-        }
+        // fn get(&self, id: Thing) -> Result<Task<Thing>> {
+        //     self.rt
+        //         .block_on(
+        //             self.db
+        //                 .select((id.tb.clone(), id.id.to_raw()))
+        //                 .into_future(),
+        //         )?
+        //         .ok_or_else(|| anyhow!("Invalid task ID: {}", id))
+        // }
     }
 
     /// Instantiate an in-memory Db with `ns` & `db` = "HelixFlow".
@@ -120,53 +141,46 @@ pub mod blocking {
 
     #[cfg(test)]
     mod tests {
-        use std::str::FromStr;
         use helixflow_core::task::blocking::TaskExt;
+        use std::str::FromStr;
 
         use super::*;
 
         #[test]
         fn test_new_task_id_updated() {
             {
-                let mut new_task = Task {
-                    name: "Test Task 1".into(),
-                    id: None,
-                    description: None,
-                };
+                let new_task = Task::new("Test Task 1", None);
                 let backend = SurrealDb::create().unwrap();
-                new_task.create(&backend).unwrap(); // Unwrap to check we don't get any errors
-                assert_eq!(new_task.name, "Test Task 1");
-                assert!(new_task.description.is_none());
-                assert!(new_task.id.is_some());
+                new_task.create(&backend).unwrap();
             }
         }
 
-        #[test]
-        fn test_new_task_written_to_db() {
-            {
-                let mut new_task = Task {
-                    name: "Test Task 2".into(),
-                    id: None,
-                    description: None,
-                };
-                let backend = SurrealDb::create().unwrap();
-                new_task.create(&backend).unwrap(); // Unwrap to check we don't get any errors
-                let id = new_task.id.unwrap();
-                let stored_task: Task<Thing> = backend.get(id).unwrap();
-                assert_eq!(stored_task.name, new_task.name);
-                assert_eq!(stored_task.description, new_task.description);
-            }
-        }
+        // #[test]
+        // fn test_new_task_written_to_db() {
+        //     {
+        //         let mut new_task = Task {
+        //             name: "Test Task 2".into(),
+        //             id: None,
+        //             description: None,
+        //         };
+        //         let backend = SurrealDb::create().unwrap();
+        //         new_task.create(&backend).unwrap(); // Unwrap to check we don't get any errors
+        //         let id = new_task.id.unwrap();
+        //         let stored_task: Task<Thing> = backend.get(id).unwrap();
+        //         assert_eq!(stored_task.name, new_task.name);
+        //         assert_eq!(stored_task.description, new_task.description);
+        //     }
+        // }
 
-        #[test]
-        fn test_get_invalid_task() {
-            {
-                let backend = SurrealDb::create().unwrap();
-                let id = Thing::from_str("table:record").unwrap();
-                let err = backend.get(id).unwrap_err();
-                assert_eq!(format!("{}", err), "Invalid task ID: table:record");
-            }
-        }
+        // #[test]
+        // fn test_get_invalid_task() {
+        //     {
+        //         let backend = SurrealDb::create().unwrap();
+        //         let id = Thing::from_str("table:record").unwrap();
+        //         let err = backend.get(id).unwrap_err();
+        //         assert_eq!(format!("{}", err), "Invalid task ID: table:record");
+        //     }
+        // }
 
         // #[test]
         // fn test_new_task_written_to_external_db() {
