eb1360125fbcd14c5f7a4547c5ac180b03941e94
Mike Foster <i.am.mike.foster@gmail.com>
2025-05-20 16:11:23 +0000

    only emit debug info when testing

--- ui/helixflow-slint/build.rs
+++ ui/helixflow-slint/build.rs
@@ -1,12 +1,13 @@
-use glob::glob;
-
 fn main() {
     // Set SLINT_EMIT_DEBUG_INFO=1 for tests, rust-analyzer, etc.
     if std::env::var("DEBUG") == Ok("true".to_string()) {
+        use glob::glob;
+
         println!("cargo:rustc-env=SLINT_EMIT_DEBUG_INFO=1");
-    }
-    let config = slint_build::CompilerConfiguration::new().with_debug_info();
-    for slint_file in glob("src/*.slint").expect("Failed to find slint files") {
-        slint_build::compile_with_config(slint_file.unwrap(), config.clone()).unwrap();
+
+        let config = slint_build::CompilerConfiguration::new().with_debug_info();
+        for slint_file in glob("src/*.slint").expect("Failed to find slint files") {
+            slint_build::compile_with_config(slint_file.unwrap(), config.clone()).unwrap();
+        }
     }
 }
