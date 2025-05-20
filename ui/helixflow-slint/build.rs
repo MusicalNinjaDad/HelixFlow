91ea0e7d077f588c6c36a541e7efc4d7536d6320
Mike Foster <i.am.mike.foster@gmail.com>
2025-05-20 15:32:16 +0000

    move tasks to separate slint file

--- ui/helixflow-slint/build.rs
+++ ui/helixflow-slint/build.rs
@@ -3,4 +3,6 @@ fn main() {
     if std::env::var("DEBUG") == Ok("true".to_string()) {
         println!("cargo:rustc-env=SLINT_EMIT_DEBUG_INFO=1");
     }
+    let config = slint_build::CompilerConfiguration::new().with_debug_info();
+    slint_build::compile_with_config("src/tasks.slint", config).unwrap();
 }
