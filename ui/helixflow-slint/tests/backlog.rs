a99d1a486afe9e4074063b0fe24a3ff60492a439
Mike Foster <i.am.mike.foster@gmail.com>
2025-05-26 11:08:08 +0000

    basic shapre to init a Backlog

--- ui/helixflow-slint/tests/backlog.rs
+++ ui/helixflow-slint/tests/backlog.rs
@@ -1,5 +1,10 @@
+#![feature(cfg_boolean_literals)]
+use std::rc::Rc;
+
+use helixflow_core::task::{blocking::TestBackend, TaskList};
 use helixflow_slint::{Backlog, SlintTask, test::*};
 use slint::{ComponentHandle, ModelRc, VecModel};
+use uuid::uuid;
 
 #[test]
 fn update_tasks_in_event_loop() {
@@ -28,4 +33,18 @@ fn update_tasks_in_event_loop() {
     list_elements!(&backlog);
     let backlog_tasks = ElementHandle::find_by_element_type_name(&backlog, "TaskListItem");
     assert_values!(backlog_tasks, tasks);
+}
+
+#[test]
+#[cfg(false)]
+fn initialise_backlog() {
+    prepare_slint!();
+    
+    let backlog = Backlog::new().unwrap();
+    list_elements!(&backlog);
+
+    let backend = Rc::new(TestBackend);
+
+    let backlog_data = TaskList::get(&backend, uuid!("0196fe23-7c01-7d6b-9e09-5968eb370549"));
+    backlog.init(backlog_data);
 }
