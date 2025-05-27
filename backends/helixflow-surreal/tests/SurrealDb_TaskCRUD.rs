7fa8561802bf278bffbe103a8369aebe1e51ad49
Mike Foster <i.am.mike.foster@gmail.com>
2025-05-27 18:18:09 +0000

    test passes

--- backends/helixflow-surreal/tests/SurrealDb_TaskCRUD.rs
+++ backends/helixflow-surreal/tests/SurrealDb_TaskCRUD.rs
@@ -67,4 +67,21 @@ mod blocking {
             .collect();
         assert_eq!(tasks, vec![task]);
     }
+
+    #[test]
+    fn create_two_tasks_in_tasklist() {
+        let backend = SurrealDb::new().unwrap();
+        let tasklist = TaskList::new("Test tasklist");
+        tasklist.create(&backend).unwrap();
+        let task2 = Task::new("Test Task 2", None);
+        task2.create_linked(&backend, &tasklist).unwrap();
+        let task3 = Task::new("Test Task 3", None);
+        task3.create_linked(&backend, &tasklist).unwrap();
+        let tasks: Vec<Task> = tasklist
+            .tasks(&backend)
+            .unwrap()
+            .map(Result::unwrap)
+            .collect();
+        assert_eq!(tasks, vec![task2, task3]);
+    }
 }
